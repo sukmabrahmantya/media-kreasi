a05a545aa83d7d02f35d3d10aa4c717865bf6651
sukmabrahmantya <brahmantyasukma@gmail.com>
2025-01-08 08:24:46 +0700

    feat: service section

---
+++ src/components/Pages/Service/index.tsx
@@ -0,0 +1,121 @@
+import { Box, Text, VStack, HStack, Image, UnorderedList, ListItem } from "@chakra-ui/react";
+
+const ServiceSection = () => {
+  return (
+    <Box
+      as="section"
+      pt={10}
+      pb={12}
+      bg="#EFF4FE"
+      position="relative"
+      id="service"
+    >
+      {/* Title */}
+      <Text
+        fontSize="5rem"
+        fontWeight="400"
+        color="black"
+        fontFamily="chalkboy"
+        align="center"
+      >
+        WE PROVIDE BEST SERVICE
+      </Text>
+
+      {/* Service Layout */}
+      <Box
+        mx="auto"
+        maxW="container.xl"
+        display="flex"
+        justifyContent="space-around"
+        alignItems="center"
+        flexWrap="wrap"
+        gap={2}
+      >
+        {/* Left Section: Event & Experiential */}
+        <VStack
+          align="flex-start"
+          spacing={3}
+          w="330px"
+          alignSelf="flex-start"
+          mt={6}
+        >
+          <Text
+            fontSize="4xl"
+            fontWeight={700}
+            color="#0367AB"
+            textTransform="uppercase"
+            lineHeight="41px"
+            fontFamily="arial"
+          >
+            Event & Experiential
+          </Text>
+          <UnorderedList color="#0367AB" fontSize="xl" lineHeight="23px">
+            <ListItem>Live event, concert</ListItem>
+            <ListItem>Virtual event</ListItem>
+            <ListItem>Conference, expo, seminar gathering</ListItem>
+          </UnorderedList>
+        </VStack>
+
+        {/* Center Image */}
+        <Image
+          src="/images/equipment-2.png"
+          alt="Equipment"
+          borderRadius="md"
+          maxW="500px"
+          // maxW="530px"
+          h="auto"
+        />
+
+        {/* Right Section: Technical & Equipment */}
+        <VStack
+          align="flex-start"
+          spacing={3}
+          w="375px"
+          mt={16}
+        >
+          <Text
+            fontSize="4xl"
+            fontWeight={700}
+            color="#0367AB"
+            textTransform="uppercase"
+            lineHeight="41px"
+            fontFamily="arial"
+          >
+            Technical & Equipment
+          </Text>
+          <UnorderedList color="#0367AB" fontSize="xl" lineHeight="23px">
+            <ListItem>Soundsystem</ListItem>
+            <ListItem>Lighting, LED screen, genset</ListItem>
+            <ListItem>Rigging truss</ListItem>
+          </UnorderedList>
+        </VStack>
+
+        {/* Bottom Section: Design & Build */}
+        <VStack
+          align="flex-start"
+          spacing={3}
+          w="380px"
+          mt={4}
+          textAlign="left"
+        >
+          <Text
+            fontSize="4xl"
+            fontWeight={700}
+            color="#0367AB"
+            textTransform="uppercase"
+            lineHeight="41px"
+            fontFamily="arial"
+          >
+            Design & Build
+          </Text>
+          <UnorderedList color="#0367AB" fontSize="xl" lineHeight="23px">
+            <ListItem>Booth & backdrop production</ListItem>
+            <ListItem>3D modeling</ListItem>
+          </UnorderedList>
+        </VStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ServiceSection;
