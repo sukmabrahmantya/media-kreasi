240968d9e0972fce95abf8e8261cc74a85794988
sukmabrahmantya <brahmantyasukma@gmail.com>
2025-01-14 10:16:44 +0700

    feat: update layout our client section

--- src/components/Pages/OurClient/index.tsx
+++ src/components/Pages/OurClient/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Text, Image, HStack, useBreakpointValue, VStack } from "@chakra-ui/react";
+import { Box, Text, Image } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
 const clients = {
@@ -10,6 +10,11 @@ const clients = {
     { src: "/images/client/client-3.png", alt: "Client 3" },
     { src: "/images/client/client-4.png", alt: "Client 4" },
     { src: "/images/client/client-5.png", alt: "Client 5" },
+    { src: "/images/client/client-13.png", alt: "Client 13" },
+    { src: "/images/client/client-14.png", alt: "Client 14" },
+    { src: "/images/client/client-15.JPG", alt: "Client 15" },
+    { src: "/images/client/client-16.jpg", alt: "Client 16" },
+    { src: "/images/client/client-17.png", alt: "Client 17" },
   ],
   row2: [
     { src: "/images/client/client-6.jpeg", alt: "Client 6" },
@@ -19,15 +24,6 @@ const clients = {
     { src: "/images/client/client-10.png", alt: "Client 10" },
     { src: "/images/client/client-11.png", alt: "Client 11" },
     { src: "/images/client/client-12.png", alt: "Client 12" },
-  ],
-  row3: [
-    { src: "/images/client/client-13.png", alt: "Client 13" },
-    { src: "/images/client/client-14.png", alt: "Client 14" },
-    { src: "/images/client/client-15.JPG", alt: "Client 15" },
-    { src: "/images/client/client-16.jpg", alt: "Client 16" },
-    { src: "/images/client/client-17.png", alt: "Client 17" },
-  ],
-  row4: [
     { src: "/images/client/client-18.png", alt: "Client 18" },
     { src: "/images/client/client-19.JPG", alt: "Client 19" },
     { src: "/images/client/client-20.PNG", alt: "Client 20" },
@@ -38,11 +34,8 @@ const clients = {
 };
 
 const OurClientSection = () => {
-  const isMobile = useBreakpointValue({ base: true, md: false });
-  const combinedRow1 = [...clients.row1, ...clients.row3];
-  const combinedRow2 = [...clients.row2, ...clients.row4];
-  const infiniteRow1 = [...combinedRow1, ...combinedRow1];
-  const infiniteRow2 = [...combinedRow2, ...combinedRow2];
+  const infiniteRow1 = [...clients.row1, ...clients.row1, ...clients.row1, ...clients.row1];
+  const infiniteRow2 = [...clients.row2, ...clients.row2, ...clients.row2, ...clients.row2];
 
   const [isClient, setIsClient] = useState(false);
 
@@ -56,6 +49,7 @@ const OurClientSection = () => {
       py={{ base: 5, md: 10 }}
       id="our-client"
       bg="white"
+      mb={5}
       px={{ base: 0, md: 4 }}
       overflow="hidden"
     >
@@ -71,87 +65,57 @@ const OurClientSection = () => {
         OUR CLIENT
       </Text>
 
-      {isMobile ? (
-        isClient && (
-          <>
-            <Box
-              display="flex"
-              animation="scroll2 15s linear infinite"
-              whiteSpace="nowrap"
-              mb={4}
-            >
-              {infiniteRow1.map((client, index) => (
-                <Box
-                  key={index}
-                  mx={3}
-                  flexShrink={0}
-                  display="inline-block"
-                  w="120px"
-                >
-                  <Image
-                    src={client.src}
-                    alt={client.alt}
-                    w="auto"
-                    h="67px"
-                    objectFit="contain"
-                  />
-                </Box>
-              ))}
-            </Box>
+      {isClient && (
+        <>
+          <Box
+            display="flex"
+            animation="scroll2 15s linear infinite"
+            whiteSpace="nowrap"
+            mb={6}
+          >
+            {infiniteRow1.map((client, index) => (
+              <Box
+                key={index}
+                mx={{ base: 2, md: 3 }}
+                flexShrink={0}
+                display="inline-block"
+                w={{ base: "120px", md: "190px" }}
+              >
+                <Image
+                  src={client.src}
+                  alt={client.alt}
+                  w="auto"
+                  h={{ base: "67px", md: "110px" }}
+                  objectFit="contain"
+                />
+              </Box>
+            ))}
+          </Box>
 
-            <Box
-              display="flex"
-              animation="scroll 20s linear infinite"
-              whiteSpace="nowrap"
-            >
-              {infiniteRow2.map((client, index) => (
-                <Box
-                  key={index}
-                  mx={3}
-                  flexShrink={0}
-                  display="inline-block"
-                  h="67px"
-                >
-                  <Image
-                    src={client.src}
-                    alt={client.alt}
-                    w="auto"
-                    h="full"
-                    objectFit="contain"
-                  />
-                </Box>
-              ))}
-            </Box>
-          </>
-        )
-      ) : (
-        // Desktop: Row-based grid
-        <VStack spacing={8}>
-          {Object.values(clients).map((row, rowIndex) => (
-            <HStack
-              key={rowIndex}
-              spacing={8}
-              w="full"
-              justifyContent="space-between"
-            >
-              {row.map((client, index) => (
-                <Box
-                  key={index}
-                  h={rowIndex % 2 == 0 ? "full" : "auto"}
-                  w={rowIndex % 2 !== 0 ? "auto" : "full"}
-                >
-                  <Image
-                    src={client.src}
-                    alt={client.alt}
-                    w="100%"
-                    h="100px"
-                    objectFit="contain"
-                  />
-                </Box>
-              ))}
-            </HStack>
-          ))}
-        </VStack>
+          <Box
+            display="flex"
+            animation="scroll 20s linear infinite"
+            whiteSpace="nowrap"
+          >
+            {infiniteRow2.map((client, index) => (
+              <Box
+                key={index}
+                mx={{ base: 2, md: 6 }}
+                flexShrink={0}
+                display="inline-block"
+                h={{ base: "67px", md: "110px" }}
+              >
+                <Image
+                  src={client.src}
+                  alt={client.alt}
+                  w="auto"
+                  h="full"
+                  objectFit="contain"
+                />
+              </Box>
+            ))}
+          </Box>
+        </>
       )}
     </Box >
   );
